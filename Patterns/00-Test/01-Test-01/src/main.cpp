dea9a8af842a75ed367bcf14c9c7a4529161b05f
copyrat90 <copyrat90@gmail.com>
2021-11-30 23:43:32 +0900

    [TEST] broken PR

--- Patterns/00-Test/01-Test-01/src/main.cpp
+++ Patterns/00-Test/01-Test-01/src/main.cpp
@@ -1,61 +1,11 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
-
-#include <imconfig-SFML.h>
-#include <imgui-SFML.h>
-#include <imgui.h>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "");
-    window.setVerticalSyncEnabled(true);
-    ImGui::SFML::Init(window);
+ std::  cout << "Hello, world!" << std:: endl
+     ;
 
-    sf::Color bgColor;
-
-    float color[3] = {0.f, 0.f, 0.f};
-
-    // char buffer for now, std::string comes next tutorial
-    char windowTitle[255] = "ImGui + SFML = <3";
-
-    window.setTitle(windowTitle);
-    window.resetGLStates(); // call it if you only draw ImGui, otherwise not needed.
-    sf::Clock deltaClock;
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            ImGui::SFML::ProcessEvent(event);
-
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-        ImGui::Begin("Sample window");
-
-        if (ImGui::ColorEdit3("bg color", color))
-        {
-            bgColor.r = static_cast<sf::Uint8>(color[0] * 255.f);
-            bgColor.g = static_cast<sf::Uint8>(color[1] * 255.f);
-            bgColor.b = static_cast<sf::Uint8>(color[2] * 255.f);
-        }
-
-        ImGui::InputText("Window title", windowTitle, 255);
-
-        if (ImGui::Button("Update window title"))
-            window.setTitle(windowTitle);
-        ImGui::End();
-        ImGui::ShowDemoWindow();
-
-        window.clear(bgColor);
-        ImGui::SFML::Render(window);
-        window.display();
-    }
-
-    ImGui::SFML::Shutdown();
+    NoExistFunc();
 
     return 0;
 }
